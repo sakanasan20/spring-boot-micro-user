8120a3d3ac923372b40e4772f7fcf313921d1491
nick.chen <sakanasan20@gmail.com>
2022-07-08 15:05:01 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBootMicroUserApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroUserApplication.class, args);
+	}
+
+}
