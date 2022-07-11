5409046970959880accd3173d8a577e4562d6151
nick.chen <sakanasan20@gmail.com>
2022-07-11 11:35:54 +0800

    Create user repository

---
+++ src/main/java/tw/niq/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package tw.niq.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import tw.niq.app.entity.UserEntity;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}
