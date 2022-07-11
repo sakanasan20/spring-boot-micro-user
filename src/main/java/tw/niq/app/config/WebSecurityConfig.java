61b9c88cb18c2c55b30db620bfc82b9d8a4ee9f8
nick.chen <sakanasan20@gmail.com>
2022-07-11 13:40:38 +0800

    Add web security config

---
+++ src/main/java/tw/niq/app/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package tw.niq.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final Environment environment;
+
+	public WebSecurityConfig(Environment environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		http.authorizeRequests().antMatchers("/users/**").hasIpAddress(environment.getProperty("gateway.ip"));
+	}
+
+}
